bbdd82843b5306e6ab0b0a5238f8a621bc3c1e4e
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-01-31 16:58:08 +0300

    проверка на квадрат второго числа

---
+++ Example001/Example002_a=bb/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите второе число");
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB * numberB)
+{
+    System.Console.WriteLine("Правильно!");
+}
+else
+{
+    System.Console.WriteLine("Не подходит, пробуй еще");
+}
+
+
+
