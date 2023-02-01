4471f8808e8c63d085a97be5dec94cf5a188adbe
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-01 13:22:49 +0300

    While

---
+++ Example004_While/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int currentNumber = number  -1;
+while (currentNumber <= number)
+System.Console.WriteLine(currentNumber);
+currentNumber++;
