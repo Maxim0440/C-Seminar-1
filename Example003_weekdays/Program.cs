e46ab8da798672dab61086c0b6ce860b8b2c9780
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-01 11:23:54 +0300

    Days

---
+++ Example003_weekdays/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Введите число");
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == 1)
+{
+    System.Console.WriteLine("Это понедельник");
+}
+
+else if (numberA == 2)
+{
+    System.Console.WriteLine("Это вторник");
+}
+
+else if (numberA == 3)
+{
+    System.Console.WriteLine("Это среда");
+}
+
+else if (numberA == 4)
+{
+    System.Console.WriteLine("Это четверг");
+}
+
+else if (numberA == 5)
+{
+    System.Console.WriteLine("Это пятница");
+}
+
+else if (numberA == 6)
+{
+    System.Console.WriteLine("Это суббота");
+}
+
+else if (numberA == 7)
+{
+    System.Console.WriteLine("Это воскресенье");
+}
+{
+    System.Console.WriteLine("Это воскресенье");
+}
+
+else
+{
+    System.Console.WriteLine("Нет такого дня");
+}
+
+
