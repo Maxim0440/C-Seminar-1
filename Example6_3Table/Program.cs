a465993a30623c35c9edecaeeea7452825b1716f
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-02 14:16:11 +0300

    добавлено сравнение for и while

--- Example6_3Table/Program.cs
+++ Example6_3Table/Program.cs
@@ -4,3 +4,12 @@
 
  System.Console.WriteLine(3 + " * " + i + " = " + 3 * i);   
 }
+
+//Для while это будет так:
+//int i = 1;
+// while (i <= 10)
+// {
+    //System.Console.WriteLine(3 + " * " + i + " = " + 3 * i);
+    // i++
+
+}
